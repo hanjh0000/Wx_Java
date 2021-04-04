f309a31b96da9d72e6827b52a57de2645c6a667a
hanjinhong <jinhong.han@downjoy.com>
2021-04-04 13:33:11 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## WxJava - 微信开发 Java SDK
+
+[![Maven Central](https://img.shields.io/maven-central/v/com.github.binarywang/wx-java.svg)](http://mvnrepository.com/artifact/com.github.binarywang/wx-java)
+[![Build Status](https://travis-ci.org/Wechat-Group/WxJava.svg?branch=develop)](https://travis-ci.org/Wechat-Group/WxJava)
+
+#### Fork from [WxJava](https://github.com/Wechat-Group/WxJava)
+
+用于快速响应企业微信API更新
---
+++ weixin-java-common/src/main/java/me/chanjar/weixin/common/error/WxErrorException.java
@@ -0,0 +1,35 @@
+package me.chanjar.weixin.common.error;
+
+/**
+ * @author Daniel Qian
+ */
+public class WxErrorException extends Exception {
+  private static final long serialVersionUID = -6357149550353160810L;
+
+  private final WxError error;
+
+  public WxErrorException(String message) {
+    this(WxError.builder().errorCode(-1).errorMsg(message).build());
+  }
+
+  public WxErrorException(WxError error) {
+    super(error.toString());
+    this.error = error;
+  }
+
+  public WxErrorException(WxError error, Throwable cause) {
+    super(error.toString(), cause);
+    this.error = error;
+  }
+
+  public WxErrorException(Throwable cause) {
+    super(cause.getMessage(), cause);
+    this.error = WxError.builder().errorCode(-1).errorMsg(cause.getMessage()).build();
+  }
+
+  public WxError getError() {
+    return this.error;
+  }
+
+
+}
